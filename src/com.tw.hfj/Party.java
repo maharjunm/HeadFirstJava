0ae265c4f3b2336fb91831113c52b7bfae408975
maharjun <maharjun@thoughtworks.com>
2017-04-09 17:10:27 +0530

    Maharjun | added BeerSong code

---
+++ src/com.tw.hfj/Party.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class Party {
+	public void vuildInvite() {
+		Frame f = new Frame();
+		Label l = new Label("Party Maharjun");
+		Button b = new Button("You Bet");
+		Button b1 = new Button("Shoot Me");
+		Panel p = new Panel();
+		p.add(l);
+	}
+}
