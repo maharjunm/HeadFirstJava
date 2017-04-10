406922c22bc4ecbaa3f67c12b7f5e29e446b78cc
maharjun <maharjun@thoughtworks.com>
2017-04-10 21:40:04 +0530

    Maharjun | Added Telivision Class

---
+++ src/com.tw.hfj/Television.java
@@ -0,0 +1,20 @@
+package com.tw.hfj;
+
+import java.awt.*;
+
+public class Television {
+    Button power;
+    Button channel;
+    public void changeChannel() {
+
+    }
+
+    public void powerOFF() {
+        System.out.println("power is off");
+    }
+
+    public void powerOn() {
+        System.out.println("power is on");
+    }
+
+}
